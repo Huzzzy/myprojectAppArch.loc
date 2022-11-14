7181aa22e0069bd7ed8154dec4b72082246f12b4
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-14 12:28:10 +0300

    Создание объекта класса главного контроллера

--- www/index.php
+++ www/index.php
@@ -4,6 +4,5 @@ spl_autoload_register(function (string $className) {
     require_once '../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
-$author = new \MyProject\Models\Users\User('Иван');
-$article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
-var_dump($article);
+$controller = new \MyProject\Controllers\MainController();
+$controller->main();
